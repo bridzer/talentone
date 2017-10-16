cf3a3f8873934d9461bd0f7bee5e1717cf3122f7
bridzer <kipkemoicarlos22@gmail.com>
2017-10-16 16:55:20 +0300

    Update users_controller.rb

--- users_controller.rb
+++ users_controller.rb
@@ -75,6 +75,6 @@ class UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:firstname, :lastname, :username, :email, :password, :dateofbirth, :country, :zipcode, :city)
+      params.require(:user).permit(:firstname, :lastname, :username, :email, :password, :dateofbirth, :country, :zipcode, :city, :has_secure_password )
     end
 end
